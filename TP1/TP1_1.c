5531dfec5ed7ac0d214abf65b339df364604656b
lthuillie002 <74877406+lthuillie002@users.noreply.github.com>
2021-04-15 23:19:48 +0200

    a

--- TP1/TP1_1.c
+++ TP1/TP1_1.c
@@ -7,6 +7,7 @@ int main() {
 	FILE* cpu = NULL;	//Stockage des informations système du CPU
 	FILE* mem = NULL;	//Stockage des informations système de la mémoire
 	FILE* part = NULL;	//Stockage des informations système des partitions
+	FILE* vers = NULL;	//Stockage des informations système de la version de linux
 
 	/* Déclaration des variables*/
 	char model_name[BUFFER_SIZE];
@@ -18,6 +19,7 @@ int main() {
 	char mem_free[BUFFER_SIZE];
 	char part_size[TAB_SIZE][BUFFER_SIZE];
 	char part_name[TAB_SIZE][BUFFER_SIZE];
+	char version[BUFFER_SIZE];
 
 	char buffer[BUFFER_SIZE];
 
@@ -29,6 +31,7 @@ int main() {
 	cpu = fopen("/proc/cpuinfo", "r");
 	mem = fopen("/proc/meminfo", "r");
 	part = fopen("/proc/partitions", "r");
+	vers = fopen("/proc/version", "r");
 
 	/* Récupération des informations du CPU */
 	if (cpu == NULL)
@@ -79,21 +82,25 @@ int main() {
 			while (p != NULL)
 			{
 				col ++;
-				if (col == 3 && line > 1)
-                {
+				if (col == 3 && line > 1)	// Lecture de la taille de la partition
 					strcpy(part_size[line], p);
-					printf("size : %s\n", part_size[line]);
-                }
-				if (col == 4 && line > 1)
-                {
+				if (col == 4 && line > 1)	// Lecture du nom de la partition
 					strcpy(part_name[line], p);
-					printf("Name : %s\n", part_name[line]);
-                }
 
 				p = strtok(NULL, split);
 			}
 			line ++;
 		}
+	}
+
+	/* Récupération de la version du noyau instalé */
+	if (vers == NULL)
+	{printf("Impossible d'ouvrir le fichier");}
+	else {
+		fgets(buffer,BUFFER_SIZE,vers) != NULL;
+		char split_2 = "(";
+		char *p = strtok(buffer, split_2);
+		strcpy(version, p);
 	}
 
 	/* Affichage des résultats */
@@ -103,6 +110,7 @@ int main() {
 	printf("Partitions : \n");
 	for(i = 2; i < line; i++)
 		printf("%s\t%s Mo\n", part_name[i], part_size[i]);
+	printf("Version du noyau installé :\n\t%s\n", version);
 
 	return 0;
 }
